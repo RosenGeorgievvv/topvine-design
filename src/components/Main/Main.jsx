c5b6b3150d1abaf6de16086c7d14f90170078fc8
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-11-08 17:29:41 +0200

    Revert "styling update"
    This reverts commit 211e14c559143e01e1ea4e2daf914a81cdb928bd.

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -4,14 +4,13 @@ import main from '../../assets/main.png'
 
 const Main = () => {
   return (
-    <Box position="relative" width="auto" height="auto">
+    <Box position="relative" width="100%" height="100vh">
       <Image
         src={main}
         alt="Waterpark background"
         objectFit="cover"
         width="100%"
         height="100%"
-        margin="0"
       />
       <Box
         position="absolute"
@@ -25,7 +24,7 @@ const Main = () => {
           fontWeight="bold"
           color="rgba(0, 35, 69, 1)"
           lineHeight="1.2"
-          width="100%"
+          width="120%"
           textAlign="end"
         >
           The Aquatic Professionals <br /> Number One Choice
@@ -35,7 +34,7 @@ const Main = () => {
           fontWeight="normal"
           color="rgba(0, 35, 69, 0.7)"
           mt={2}
-          width="100%"
+          width="120%"
           textAlign="end"
         >
           For Strainers, Reducers & Float Valves
