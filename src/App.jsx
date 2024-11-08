2522d9668fc41b1a3803e2b54a98e11741141603
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-11-08 10:32:16 +0200

    Project setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Navbar from './components/Navigation/Navbar'
+
+function App() {
+
+  return (
+    <>
+     <Navbar />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navigation/Navbar.jsx
@@ -0,0 +1,13 @@
+import { Button } from "../ui/button"
+import { HStack } from "@chakra-ui/react"
+
+const Navbar = () => {
+  return (
+    <HStack>
+      <Button>Click me</Button>
+      <Button>Click me</Button>
+    </HStack>
+  )
+}
+
+export default Navbar;
